aa5deb2dfa8f0ce6e664844d2bb3cba5005134ae
Jon Robison <narfman0@gmail.com>
2018-11-29 12:55:57 -0500

    -Add docstrings for parse_schema

--- bigquery_schema_coerce/core.py
+++ bigquery_schema_coerce/core.py
@@ -5,7 +5,15 @@ from google.cloud.bigquery import schema
 
 def parse_schema(schema_dict=None, text=None, path=None):
     """ Parse a schema at the given path.
-    return: a generator of SchemaFields
+    Caller may pass a path to the schema using the path argument,
+    the text of a schema using the text argument, or the parsed
+    schema in the form of a dict to the schema_dict argument.
+
+    :param dict schema_dict: Parsed schema, like from json.loads('...')
+    :param str text: Schema as a json string
+    :param str path: Path to a json schema
+    :return: a list of SchemaFields
+    :rtype: generator of SchemaFields
     """
     if path:
         with open(path) as schema_file:
